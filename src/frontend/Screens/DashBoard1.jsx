8e53ec53c98861b50291c5a30da90f84e03500be
franbz1 <franciscorualesp15@gmail.com>
2024-10-27 17:23:13 -0500

    Optimizacion dashboard

--- src/frontend/Screens/DashBoard1.jsx
+++ src/frontend/Screens/DashBoard1.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, FlatList } from 'react-native';
+import { Alert, FlatList, ActivityIndicator, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { deleteProject, subscribeToProjects } from '../../Backend/services/ProjectoService';
 import ProjectCard from '../components/Dahsboard1/projectCard';
@@ -10,39 +10,49 @@ import BarraOpciones from '../components/BarraOpciones';
 export default function DashBoard1() {
   const [projects, setProjects] = useState([]);
   const [isProjectModalVisible, setIsProjectModalVisible] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = subscribeToProjects(setProjects);
+    const unsubscribe = subscribeToProjects((projectsData) => {
+      setProjects(projectsData);
+      setLoading(false);
+    });
+
     return () => unsubscribe();
   }, []);
 
   const handleDeleteProject = async (projectId) => {
     try {
       await deleteProject(projectId);
-    } catch (error) {
+    } catch {
       Alert.alert('Error', 'No se pudo eliminar el proyecto.');
     }
   };
 
-  const handleAddProject = (project) => {
-    setProjects(prevProjects => [...prevProjects, project]);
-  };
+  const handleAddProject = (newProject) => setProjects(prevProjects => [...prevProjects, newProject]);
 
   return (
     <SafeAreaView className="flex-1 bg-black">
       <BarraOpciones />
-      <FlatList
-        data={[...projects, { id: 'add', name: 'Añadir Proyecto' }]}
-        renderItem={({ item }) =>
-          item.id === 'add' ? (
-            <AddProjectCard onAdd={() => setIsProjectModalVisible(true)} />
-          ) : (
-            <ProjectCard project={item} onDelete={handleDeleteProject} />
-          )
-        }
-        keyExtractor={(item) => item.id}
-        numColumns={2}
-      />
+      {loading ? (
+        <View className="flex-1 justify-center items-center">
+          <ActivityIndicator size="large" color="#FFC107" />
+        </View>
+      ) : (
+        <FlatList
+          data={[...projects, { id: 'add', name: 'Añadir Proyecto' }]}
+          renderItem={({ item }) =>
+            item.id === 'add' ? (
+              <AddProjectCard onAdd={() => setIsProjectModalVisible(true)} />
+            ) : (
+              <ProjectCard project={item} onDelete={handleDeleteProject} />
+            )
+          }
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={2}
+          contentContainerStyle={{ paddingHorizontal: 8 }}
+        />
+      )}
       <ModalCrearProyecto
         visible={isProjectModalVisible}
         onClose={() => setIsProjectModalVisible(false)}
