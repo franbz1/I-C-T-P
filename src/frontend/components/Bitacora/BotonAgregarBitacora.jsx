9460272e16048c0b26d7e8956a05b29f3d5ca6d0
franbz1 <franciscorualesp15@gmail.com>
2024-10-27 23:04:45 -0500

    Boton agregar bitacora

--- src/frontend/components/Bitacora/BotonAgregarBitacora.jsx
+++ src/frontend/components/Bitacora/BotonAgregarBitacora.jsx
@@ -1,53 +1,28 @@
-import React, { useState } from 'react';
+import React, { useCallback } from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { styled } from 'nativewind';
 import * as Animatable from 'react-native-animatable';
 
-/* se crean animaciones para los botones */
 const AnimatedTouchableOpacity = Animatable.createAnimatableComponent(TouchableOpacity);
 const Button = styled(AnimatedTouchableOpacity);
 const ButtonText = styled(Animatable.Text);
 
-/* Boton agregar */
 const BotonAgregarBitacora = ({ id }) => {
   const navigation = useNavigation();
-  const [isPressed, setIsPressed] = useState(false);
 
-  /* boton presionado */
-  const handlePress = () => {
-    setIsPressed(true);
-    navigation.navigate('FormularioBitacora', {id: id});
-  };
+  const handlePress = useCallback(() => {
+    navigation.navigate('FormularioBitacora', { id });
+  }, [navigation, id]);
 
-/* funcion de animacion */
-  const handlePressIn = () => {
-    setIsPressed(true);
-  };
-
-  const handlePressOut = () => {
-    setIsPressed(false);
-  };
-
-  /* render del boton */
   return (
     <Button
       className="bg-yellow-400 py-2 px-4 rounded-md items-center my-2 mx-3"
       onPress={handlePress}
-      onPressIn={handlePressIn}
-      onPressOut={handlePressOut}
-      animation={isPressed ? "pulse" : undefined}
-      iterationCount={1}
-      duration={300}
+      onPressIn={(e) => e.target.animate({ 0: { scale: 1 }, 1: { scale: 0.9 } }, 100)}
+      onPressOut={(e) => e.target.animate({ 0: { scale: 0.9 }, 1: { scale: 1 } }, 100)}
     >
-      <ButtonText
-        className="text-black font-bold text-lg"
-        animation={isPressed ? "rubberBand" : undefined}
-        iterationCount={1}
-        duration={300}
-      >
-        + Añadir Bitácora
-      </ButtonText>
+      <ButtonText className="text-black font-bold text-lg">+ Añadir Bitácora</ButtonText>
     </Button>
   );
 };
