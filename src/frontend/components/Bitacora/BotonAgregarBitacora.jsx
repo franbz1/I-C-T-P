46bf6085910de9392a65f8187bd14ecdf79b58f7
franbz1 <franciscorualesp15@gmail.com>
2024-10-27 23:06:43 -0500

    Revert cambios boton

--- src/frontend/components/Bitacora/BotonAgregarBitacora.jsx
+++ src/frontend/components/Bitacora/BotonAgregarBitacora.jsx
@@ -1,28 +1,53 @@
-import React, { useCallback } from 'react';
+import React, { useState } from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { styled } from 'nativewind';
 import * as Animatable from 'react-native-animatable';
 
+/* se crean animaciones para los botones */
 const AnimatedTouchableOpacity = Animatable.createAnimatableComponent(TouchableOpacity);
 const Button = styled(AnimatedTouchableOpacity);
 const ButtonText = styled(Animatable.Text);
 
+/* Boton agregar */
 const BotonAgregarBitacora = ({ id }) => {
   const navigation = useNavigation();
+  const [isPressed, setIsPressed] = useState(false);
 
-  const handlePress = useCallback(() => {
-    navigation.navigate('FormularioBitacora', { id });
-  }, [navigation, id]);
+  /* boton presionado */
+  const handlePress = () => {
+    setIsPressed(true);
+    navigation.navigate('FormularioBitacora', {id: id});
+  };
 
+/* funcion de animacion */
+  const handlePressIn = () => {
+    setIsPressed(true);
+  };
+
+  const handlePressOut = () => {
+    setIsPressed(false);
+  };
+
+  /* render del boton */
   return (
     <Button
       className="bg-yellow-400 py-2 px-4 rounded-md items-center my-2 mx-3"
       onPress={handlePress}
-      onPressIn={(e) => e.target.animate({ 0: { scale: 1 }, 1: { scale: 0.9 } }, 100)}
-      onPressOut={(e) => e.target.animate({ 0: { scale: 0.9 }, 1: { scale: 1 } }, 100)}
+      onPressIn={handlePressIn}
+      onPressOut={handlePressOut}
+      animation={isPressed ? "pulse" : undefined}
+      iterationCount={1}
+      duration={300}
     >
-      <ButtonText className="text-black font-bold text-lg">+ Añadir Bitácora</ButtonText>
+      <ButtonText
+        className="text-black font-bold text-lg"
+        animation={isPressed ? "rubberBand" : undefined}
+        iterationCount={1}
+        duration={300}
+      >
+        + Añadir Bitácora
+      </ButtonText>
     </Button>
   );
 };
