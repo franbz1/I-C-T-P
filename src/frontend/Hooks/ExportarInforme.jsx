6ac58d0910403090c2b3130f481459fb4904dc58
franbz1 <franciscorualesp15@gmail.com>
2024-10-28 02:06:58 -0500

    cambio template html

--- src/frontend/Hooks/ExportarInforme.jsx
+++ src/frontend/Hooks/ExportarInforme.jsx
@@ -102,7 +102,6 @@ function useExportarInforme() {
               height: auto;
               border-radius: 8px;
               margin: 10px 0;
-              box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
             }
           </style>
         </head>
